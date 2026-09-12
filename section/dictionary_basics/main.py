fef7a59a803e1b65bf15ba2bfca1f24e51f2dc0b
danielbappah <>
2026-09-12 23:01:11 +0000

    Dictionary Basics

---
+++ section/dictionary_basics/main.py
@@ -0,0 +1,9 @@
+# Write your code here
+library = {
+    "1": 'The Great Gatsby | F. Scott Fitzgerald',
+    "2": '1984 | George Orwell',
+    "3": 'To Kill a Mockingbird | Harper Lee'
+    }
+
+# Testing
+print('Library dictionary:', library)
